c2bfe319c805444a7fa854b20a37e51d023d3ca3
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2024-05-22 09:31:47 +0100

    Hook up app

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/scripts");
   eleventyConfig.addPassthroughCopy("./src/images");
+  eleventyConfig.addPassthroughCopy("./src/.well-known");
 
   return {
     dir: {
