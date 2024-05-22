b213c97edce6a25b14c4dd3f6befbe9b04049913
Graham Lewis <44037625+gclssvglx@users.noreply.github.com>
2024-05-22 11:00:59 +0100

    Configure content-type

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/scripts");
   eleventyConfig.addPassthroughCopy("./src/images");
   eleventyConfig.addPassthroughCopy("./src/.well-known");
+  eleventyConfig.addPassthroughCopy("./src/_headers");
 
   return {
     dir: {
